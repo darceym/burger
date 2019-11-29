41fd0b430b0fc60a8d1fe1e815cbfbbfa04ae7eb
darceym <darcey.meus@gmail.com>
2019-11-28 22:45:16 -0500

    wrote the seed.sql file to insert values into the burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger'); 
+
+INSERT INTO burgers (burger_name) VALUES ('Black bean burger'); 
